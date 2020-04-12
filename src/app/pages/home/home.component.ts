e3946b70e7274b5eaebb4d67a260ea779c98124c
fanfanqian <fanfanqian@fanfanqians-mbp.home>
2020-04-12 16:37:38 -0400

    insAPI

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Instafeed from 'instafeed.js';
 
 @Component({
   templateUrl: './home.component.html',
@@ -12,3 +13,11 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
+
+const feed =  new Instafeed({
+  // tslint:disable-next-line:max-line-length
+  accessToken: 'IGQVJXMGYxcThwa2NKM2FPeGxPcWJ3cmFVREcxeXoxem85YjViXy1mMXl5SWJXVC11QlphRk1mQmVITzA5OTF0eTQ5eUdjNWdyMTg3VEFnbGxNcU5QYzBqTXlRZA1hOVWZAHa3RBVGxiMTRTLTdHNElyTQZDZD'
+});
+feed.run();
+
