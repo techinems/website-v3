bd8ec4df1b35f09e115abc402a3393d68c319b34
fanfanqian <fanfanqian@fanfanqians-MacBook-Pro.local>
2020-04-12 23:19:15 -0400

    insAPI_update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import * as Instafeed from 'instafeed.js';
+declare var Instafeed: any;
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  board: any;
   constructor() { }
 
   ngOnInit() {
+    this.board = new Instafeed({
+      // tslint:disable-next-line:max-line-length
+      accessToken: 'IGQVJXMGYxcThwa2NKM2FPeGxPcWJ3cmFVREcxeXoxem85YjViXy1mMXl5SWJXVC11QlphRk1mQmVITzA5OTF0eTQ5eUdjNWdyMTg3VEFnbGxNcU5QYzBqTXlRZA1hOVWZAHa3RBVGxiMTRTLTdHNElyTQZDZD'
+    });
+    this.board.run();
   }
 
 }
 
 
-const feed =  new Instafeed({
-  // tslint:disable-next-line:max-line-length
-  accessToken: 'IGQVJXMGYxcThwa2NKM2FPeGxPcWJ3cmFVREcxeXoxem85YjViXy1mMXl5SWJXVC11QlphRk1mQmVITzA5OTF0eTQ5eUdjNWdyMTg3VEFnbGxNcU5QYzBqTXlRZA1hOVWZAHa3RBVGxiMTRTLTdHNElyTQZDZD'
-});
-feed.run();
+
 
